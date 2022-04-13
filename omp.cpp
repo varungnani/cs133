29f88f83b1546b9805784adca6e169c2ae9884d0
varungnani <30669335+varungnani@users.noreply.github.com>
2022-04-12 22:10:03 -0700

    Update omp.cpp

--- omp.cpp
+++ omp.cpp
@@ -20,7 +20,7 @@ void GemmParallel(const float a[kI][kK], const float b[kK][kJ],
   int i;
   int j;
   int k;
-  #pragma omp parallel for shared(A,B,C) private(i,j,k) schedule(guided)
+  #pragma omp parallel for shared(a,b,c) private(i,j,k) schedule(guided)
   for (i = 0; i < kI; ++i) {
     for (j = 0; j < kJ; ++j) {
       for (k = 0; k < kK; ++k) {
